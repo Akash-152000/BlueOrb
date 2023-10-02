869d33ed7d40dac7c0188a49e0c8e2eae5f2134e
Akash-152000 <akash152000akash@gmail.com>
2023-10-02 19:40:05 +0530

    Added div of 20 px height to move computer canvas below.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,13 +20,18 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, we`are <span className='text-[#915EFF]'>orb</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} mt-2 text-white-100 z-40`}>
             We develop 3D visuals, user <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      <div className='h-[20px]'>
+
+      </div>
+      
+        <ComputersCanvas />
+   
 
       {/*---for scroll sign------ */}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
@@ -34,16 +39,16 @@ const Hero = () => {
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             {/*-----for animation of the scrolldown */}
-            <motion.div 
-            animate={{
-          y:[0,24,0]
-            }}
-            transition={{
-              duration:1.5,
-              repeat:Infinity,
-              repeatType:'loop'
-            }}
-            className='w-3 h-3 rounded-full bg-secondary mb-1'/>
+            <motion.div
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1' />
           </div>
         </a>
       </div>
