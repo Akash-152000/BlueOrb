eba580b8c4308e5a06cbfd9aa390af26b0ee9326
Akash-152000 <akash152000akash@gmail.com>
2023-10-06 21:10:23 +0530

    made tech balls responsive

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,4 +1,4 @@
-import React,{Suspense} from 'react';
+import React, { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import {
   Decal,
@@ -11,30 +11,30 @@ import CanvasLoader from '../Loader';
 
 
 const Ball = (props) => {
-     const [decal] = useTexture([props.imgUrl]);
+  const [decal] = useTexture([props.imgUrl]);
 
   return (
-<Float speed={1.75} rotationIntensity={1}
-  floatIntensity={2}>
-    {/*---controls the contrast */}
-  <ambientLight intensity={0.25} />
-  {/*--direction of the light */}
-  <directionalLight position={[0,0,0.05]}/>
-  
-  <mesh castShadow receiveShadow scale={2.75}>
-    
-    {/*-----for the rotation----- */}
-  <icosahedronGeometry args={[1, 1]} />
+    <Float speed={1.75} rotationIntensity={1}
+      floatIntensity={2}>
+      {/*---controls the contrast */}
+      <ambientLight intensity={0.25} />
+      {/*--direction of the light */}
+      <directionalLight position={[0, 0, 0.05]} />
 
-    {/*------for the material---- */}
-    <meshStandardMaterial
+      <mesh castShadow receiveShadow scale={2.75}>
+
+        {/*-----for the rotation----- */}
+        <icosahedronGeometry args={[1, 1]} />
+
+        {/*------for the material---- */}
+        <meshStandardMaterial
           color='#fff8eb'
           polygonOffset
           polygonOffsetFactor={-5}
           flatShading
         />
         {/*---for placing the contents inside the balls */}
-         <Decal
+        <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
@@ -42,26 +42,29 @@ const Ball = (props) => {
           flatShading
         />
 
-  </mesh>
-</Float>
+      </mesh>
+    </Float>
   )
 }
 
-const BallCanvas = ({icon})=>{
-  return(
-    <Canvas
-    frameloop="demand"
-    gl={{ preserveDrawingBuffer: true }}>
+const BallCanvas = ({ icon }) => {
+  return (
+    <>
+      {window.innerWidth > 700 ? <Canvas
+        frameloop="demand"
+        gl={{ preserveDrawingBuffer: true }}>
 
-    <Suspense fallback={<CanvasLoader />}>
-      <OrbitControls
-        enableZoom={false}
-        
-      />
-      <Ball imgUrl={icon}/>
-    </Suspense>
-    <Preload all />
-  </Canvas>
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            enableZoom={false}
+
+          />
+          <Ball imgUrl={icon} />
+        </Suspense>
+        <Preload all />
+      </Canvas> : <><img src={icon} alt="tect-icon" /></>}
+    </>
+
   )
 }
 export default BallCanvas
