777828da9f48c3943f79a7da456dae36e34dcf36
Akash-152000 <akash152000akash@gmail.com>
2023-10-06 20:46:29 +0530

    Added privacy policy

--- src/App.jsx
+++ src/App.jsx
@@ -27,24 +27,24 @@ function App() {
 
 
   return (
-    
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
-        <div className='relative z-0  '>
-          <Contact />
-          <StarsCanvas />
-        </div>
-        <div>
-          <Footer/>
-        </div>
+
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
       </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <div className='relative z-0  '>
+        <Contact />
+        <StarsCanvas />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
